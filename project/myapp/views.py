ed35e572de934e6daea9ff28dfb83c1026a2df9b
Viplovesana <viplovesana90@gmail.com>
2025-06-11 05:27:33 +0530

    initial commit

---
+++ project/myapp/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(req):
+      return render(req,'home.html')
+
+def data(req):
+      if req.method == "POST":
+            n =req.POST.get('x')
+            e =req.POST.get('y')
+            c =req.POST.get('z')
+            i =req.FILES.get('f')
+            d =req.FILES.get('d')
+            data={
+                  'name':{'key1':n},
+                  'email':{'key2':e},
+                  'contact':c,
+                  'image':i,
+                  'document':d
+
+            }
+            print(n,e,c,i,d) 
+            return render(req,'data.html',{'key':data})
+      # print(req.POST)
+      # print(req.FILES)
+      # print(req.method)
+      
